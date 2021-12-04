5a24884f2577a0530cefafe234ae893bf4181e20
vkill <vkill.net@gmail.com>
2021-12-04 11:32:25 +0800

    Tweak

--- jwk-set/src/lib.rs
+++ jwk-set/src/lib.rs
@@ -23,3 +23,53 @@ impl std::str::FromStr for JsonWebKeySet {
         serde_json::from_str(json)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_de_with_apple_oidc() {
+        let jwk_set = serde_json::from_str::<JsonWebKeySet>(include_str!(
+            "../tests/oidc_keys_json_files/apple.json"
+        ))
+        .unwrap();
+        assert_eq!(jwk_set.keys.len(), 3);
+    }
+
+    #[test]
+    fn test_de_with_gitlab_oidc() {
+        let jwk_set = serde_json::from_str::<JsonWebKeySet>(include_str!(
+            "../tests/oidc_keys_json_files/gitlab.json"
+        ))
+        .unwrap();
+        assert_eq!(jwk_set.keys.len(), 2);
+    }
+
+    #[test]
+    fn test_de_with_google_oidc() {
+        let jwk_set = serde_json::from_str::<JsonWebKeySet>(include_str!(
+            "../tests/oidc_keys_json_files/google.json"
+        ))
+        .unwrap();
+        assert_eq!(jwk_set.keys.len(), 3);
+    }
+
+    #[test]
+    fn test_de_with_microsoft_oidc() {
+        let jwk_set = serde_json::from_str::<JsonWebKeySet>(include_str!(
+            "../tests/oidc_keys_json_files/microsoft.json"
+        ))
+        .unwrap();
+        assert_eq!(jwk_set.keys.len(), 7);
+    }
+
+    #[test]
+    fn test_de_with_yahoo_oidc() {
+        let jwk_set = serde_json::from_str::<JsonWebKeySet>(include_str!(
+            "../tests/oidc_keys_json_files/yahoo.json"
+        ))
+        .unwrap();
+        assert_eq!(jwk_set.keys.len(), 3);
+    }
+}
